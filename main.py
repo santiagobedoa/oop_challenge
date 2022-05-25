fd34068fda2bed7a41c86bb9b58953d36d8a12c2
yonroa <yonanderson11@gmail.com>
2022-05-25 13:58:43 -0500

    Release day

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pypokedex as pokedex
 
 
 Pokemon = __import__('pokemon').Pokemon
-A = pokedex.get(dex=np.random.randint(1, 100))
-B = pokedex.get(dex=np.random.randint(1, 100))
+A = pokedex.get(dex=np.random.randint(1, 898))
+B = pokedex.get(dex=np.random.randint(1, 898))
 
 Pokemon_A = Pokemon(A.name, A.dex, A.types[0], A.height, A.weight, A.base_stats[0], A.base_stats[1])
 Pokemon_B = Pokemon(B.name, B.dex, B.types[0], B.height, B.weight, B.base_stats[0], B.base_stats[1])
